93c8b8765393ea991b421081a0e4f5b3417a4978
Augusto Yuudi Shiraishi <augustoyuudi@gmail.com>
2021-06-20 19:17:39 -0300

    WIP pass 2 assembler

--- Assembler.java
+++ Assembler.java
@@ -125,6 +125,66 @@ public class Assembler {
   }
 
   private void passTwo() {
+    Integer currentIndex = 0;
+    String line = intermediateFile[0];
+    String[] words = line.split(whiteSpaceRegex);
+
+    if (words.length > 2) {
+      opcode = words[1];
+      String operand = words[2];
+
+      if (opcode.equals("START")) {
+        // write listing line
+        // next input line
+      }
+
+      if (!opcode.equals("START")) {
+        // write header record to object program
+        // initialize first text record
+      }
+    }
+
+    words = lines[currentIndex].split(whiteSpaceRegex);
+    opcode = words[0];
+
+    while (!opcode.equals("END")) {
+      if (opcode.charAt(0) == '.') {
+        // write listing line
+        // read next input file
+        continue;
+      }
+
+      if (optab.containsKey(opcode)) {
+        if (words.length == 3 && !words[2].equals("COMMENT")) {
+          // search symbol operand field
+          // search symtab for operand
+          // if found store symbol value as operand address
+
+          // else store 0 as operand address
+          // set error flag (undeifned symbol)
+        }
+        // store 0 as operand address
+        // assemble the object code instruction
+      }
+
+      if (opcode.equals("BYTE") || opcode.equals("WORD")) {
+        // convert constant to object code
+      }
+
+      // if object code dont fit into the current text record
+        // write text record to object program
+        // initialize new text record
+
+      // add object code to text record
+      // write listing line
+      // read next input line
+    }
+
+    // write last text record to object program
+    // write end record to object program
+    // write last listing line
+
+
     return;
   }
 }
